9fbf132543b87aae71faaf3d5533993c67a12612
Mr WhiteKidd <austinslater58@gmail.com>
2022-04-28 01:34:44 -0500

    added state to button in NavTabs NavTabs styling navTab prop

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import { Button } from '@mui/material';
+import React, { useState } from 'react';
+// import { link } from 'react-router-dom';
+
 
 
 
 function NavTabs({ currentPage, handlePageChange }) {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+
+  }
+
+  window.addEventListener('resize', showButton);
   return (
+    <div>
     <ul className="nav nav-tabs">
       <li className="nav-item">
         <a
@@ -62,6 +83,8 @@ function NavTabs({ currentPage, handlePageChange }) {
         </a>
       </li>
     </ul>
+    {button && <Button buttonStyle='btn--outine'>SIGN UP</Button>}
+  </div>
   );
 }
 
