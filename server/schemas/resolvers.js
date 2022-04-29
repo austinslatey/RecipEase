569275c1d1cb18df4fc124261ff2102f2a599daf
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-04-28 19:40:01 -0500

    added mroe mutations and queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,12 +6,25 @@ const resolvers = {
     Query: {
         user: async (parent, args) => {
             return await User.find();
-        }
+        },
+        recipe: async (parent, { _id }) => {
+            return await Recipe.findById(_id);
+          },
     },
     Mutation: {
         addUser: async (parent, args) => {
           const user = await User.create(args);
-          return { user };
+          return user ;
+        },
+        addRecipe: async (parent, args) => {
+            const recipe = await Recipe.create(args);
+            return recipe;
+        },
+        updateUser: async (parent, args) => {
+            return await User.findByIdAndUpdate(user._id, args, { new: true });
+        },
+        updateRecipe: async (parent, args) => {
+            return await Recipe.findByIdAndUpdate(recipe._id, args, { new: true });
         }
     }
 }
