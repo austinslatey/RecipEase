7112d9bcfee247fd537592fd16744b5e46461290
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-04-30 15:17:50 -0500

    contact us link opens modal with developers' email addresses

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,9 +4,28 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Container from '@mui/material/Container';
+import Modal from '@mui/material/Modal'
+import { Typography } from '@mui/material';
+import { Link } from '@mui/material';
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 export default function Footer() {
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
         <Box
             sx={{
@@ -22,7 +41,7 @@ export default function Footer() {
                     py: 3,
                     px: 2,
                     mt: 'auto',
-                    textAlign:'center',
+                    textAlign: 'center',
                     backgroundColor: (theme) =>
                         theme.palette.mode === 'light'
                             ? theme.palette.grey[200]
@@ -31,12 +50,36 @@ export default function Footer() {
             >
                 <Container maxWidth="xs">
                     <ButtonGroup variant="text" aria-label="text button group">
-                        <Button>Contact Us</Button>
+                        <Button onClick={handleOpen}>Contact Us</Button>
                         <Button target="_blank" href='https://github.com/austinslatey/RecipEase'>GitHub</Button>
                         <Button>Download</Button>
                     </ButtonGroup>
                 </Container>
+                <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby='modal-modal-title'
+                aria-describedby='modal-modal-description'
+                >
+                    <Box sx={style}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Developer E-Mail Addresses
+                        </Typography>
+                        <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+                            Johnathan Shields : <Link href="mailto:johnathan.shields.95@gmail.com">johnathan.shields.95@gmail.com</Link>
+                        </Typography>
+                        <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+                            Kyler Mclachlan : <Link href="mailto:kyler.mclachlan@gmail.com">kyler.mclachlan@gmail.com</Link>
+                        </Typography>
+                        <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+                            Austin Slatey : <Link href="mailto:austinslater58@gmail.com">austinslater58@gmail.com</Link>
+                        </Typography>
+                        <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+                            Tony Byers : <Link href="mailto:anthonyw.byers@gmail.com">anthonyw.byers@gmail.com</Link>
+                        </Typography>
+                    </Box>
+                </Modal>
             </Box>
-         </Box>
+        </Box>
     );
 }
