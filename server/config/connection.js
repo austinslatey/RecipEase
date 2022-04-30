3f68ac26ac28324c0685f6b752beb21ff0bedc74
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-04-30 12:23:40 -0500

    fixing connection.js file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const path = require('path')
 require('dotenv').config();
-const uri = "mongodb+srv://test:kylercole@cluster0.q5hmm.mongodb.net/test"
+
 
 mongoose.connect(uri|| 'mongodb://localhost/', {
   useNewUrlParser: true,
