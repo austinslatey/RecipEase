a4ae1e2511cf9afce66c697c8f02591b8b306ce4
Mr WhiteKidd <austinslater58@gmail.com>
2022-05-05 03:37:47 -0500

    implented MUI into dashboard

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,9 +1,12 @@
-import React from "react";
+import * as React from "react";
+import Card from "@mui/material/Card";
+// import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+//import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-
-import { useQuery } from '@apollo/client';
-import { QUERY_RECIPES } from '../../utils/queries';
-
+import { useQuery } from "@apollo/client";
+import { QUERY_RECIPES } from "../../utils/queries";
 
 export default function Dashboard() {
   const { data } = useQuery(QUERY_RECIPES);
@@ -11,36 +14,38 @@ export default function Dashboard() {
   console.log(recipes);
 
   return (
-    <div style={{minHeight:"calc(100vh - 40px)"}}>
+    <div style={{ minHeight: "calc(100vh - 40px)" }}>
       <h1>Dashboard</h1>
-      {recipes?.map(recipe => (
-      <div>
-        <h2>Name: {recipe.name}</h2>
-        <ul>
-          <li>
-            <h3>Instructions:</h3>
-            <p>{recipe.instructions}</p>
-          </li>
-          <li>
-            <h3>Tags:</h3>
-            <p>{recipe.tags}</p>
-          </li>
-          <li>
-            <h3>Servings:</h3>
-            <p>{recipe.servings}</p>
-          </li>
-          <li>
-            <h3>Total Time:</h3>
-            <p>{recipe.time}</p>
-          </li>
-          <li>
-            <h3>Ingredients:</h3>
-            <p>{recipe.ingredients}</p>
-          </li>
-        </ul>
-      </div>
-      ))}
+      
+        {recipes?.map((recipe) => (
+          <Card sx={{ minWidth: 275 }}>
+          
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {recipe.name}
+              </Typography>
+
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Instructions: {recipe.instructions}
+              </Typography>
+
+              <Typography variant="body2">
+                Tags:{recipe.tags}
+                <br />
+                Servings:
+                {recipe.servings}
+                <br />
+                Total Time:{recipe.time}
+                Ingredients: {recipe.ingredients}
+              </Typography>
+            </CardContent>
+            {/* plans on future dev, code is in seperate branch */}
+            {/* <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions> */}
+      </Card>
+        ))}
+      
     </div>
-    );
-    
-} 
+  );
+}
