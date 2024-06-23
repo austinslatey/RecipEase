baf4996a4929fb841edccd72ccc700739185a372
Austin Slater <austinslater58@gmail.com>
2024-06-23 02:40:58 -0500

    changed dist to build in server/server.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
+
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/RecipEase',
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
